a097d1801ccddf34802aa97545d12486cdc56ecb
kevinatown <kevinatown@gmail.com>
2018-04-29 21:28:21 -0400

    updated system start

--- setup/checkSystems.sh
+++ setup/checkSystems.sh
@@ -4,33 +4,33 @@
 
 which dnsmasq 1>/dev/null 
 if [ $? == 0 ]; then
-  echo "dnsmasq is installed\n" 
+  echo "dnsmasq is installed" 
 else
-  echo "dnsmasq is not installed, installing it and hostapd now\n"
-  source /home/pi/piController/setup/wifiSetup.sh
+  echo "dnsmasq is not installed, installing it and hostapd now"
+  source /piController/setup/wifiSetup.sh
 fi
 
 pip show gopigo 1>/dev/null 
 if [ $? == 0 ]; then
-  echo "gopigo is installed\n" 
+  echo "gopigo is installed" 
 else
-  echo "gopigo is not installed, installing it and hostapd now\n"
-  source /home/pi/piController/setup/gpg.sh
+  echo "gopigo is not installed, installing it and hostapd now"
+  source /piController/setup/gpg.sh
 fi
 
 # ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` 1>/dev/null
 grep 'wifi' /continue.conf 1>/dev/null
 if [ $? == 0 ]; then
-  echo "connected to the internet\n"
+  echo "connected to the internet"
   systemctl stop hostapd
   systemctl stop dnsmasq
 
   ifdown wlan0 && ifup wlan0
   wpa_cli -i wlan0 reconfigure
 else
-  echo "not connected to the internet\n"
+  echo "not connected to the internet"
   echo "Starting hotspot"
-  cp /home/pi/piController/configs/interfaces.hotspot /etc/network/interfaces
+  cp /piController/configs/interfaces.hotspot /etc/network/interfaces
   systemctl start hostapd
   systemctl start dnsmasq
   # systemctl restart networking
@@ -38,5 +38,5 @@ else
   # wpa_cli -i wlan0 reconfigure
 
   echo "Running the piController app"
-  /home/pi/piController/piController/main.py
+  /piController/piController/main.py
 fi
